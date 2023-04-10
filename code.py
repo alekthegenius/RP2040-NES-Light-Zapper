77692dfbafc26b982d0855f760a5bac62bcffe55
Alek V <alekvasek@icloud.com>
2023-04-09 20:11:58 -0500

    Add files via upload

---
+++ code.py
@@ -0,0 +1,50 @@
+import time
+import board
+import adafruit_mpu6050
+import busio
+import usb_hid
+from adafruit_hid.mouse import Mouse
+from digitalio import DigitalInOut, Direction, Pull
+import simpleio
+
+btn = DigitalInOut(board.GP2)
+btn.direction = Direction.INPUT
+btn.pull = Pull.UP
+
+m = Mouse(usb_hid.devices)
+i2c = busio.I2C(board.GP1, board.GP0)  # uses board.SCL and board.SDA
+mpu = adafruit_mpu6050.MPU6050(i2c)
+
+simpleio.tone(board.GP3, 1000, duration=2.0)
+time.sleep(2)
+
+m.move(x=-1920, y=1080)
+
+while True:
+    #print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2"%(mpu.acceleration))
+    #print("Gyro X:%.2f, Y: %.2f, Z: %.2f degrees/s"%(mpu.gyro))
+    #print("Temperature: %.2f C"%mpu.temperature)
+    #print("")
+    #print(btn.value)
+    y = int((mpu.gyro[2])*45) + 0
+    x = int((mpu.gyro[1])*45) + 1
+    
+    #print("Y: {}".format(y))
+    #print("X: {}".format(x))
+    if (x >=2 or x <= -2)  and (y >=2 or y <= -2):
+        m.move(x=x, y=-y)
+    
+    if btn.value == False:
+        #if btn.value == True:
+        m.click(Mouse.LEFT_BUTTON)
+        simpleio.tone(board.GP3, 5000, duration=.25)
+        time.sleep(0.1)
+        #if btn.value == False:
+           # simpleio.tone(board.GP3, 500, duration=.5)
+           # m.click(Mouse.RIGHT_BUTTON)
+           # time.sleep(1)
+           # if btn.value == False:
+            #    simpleio.tone(board.GP3, 1000, duration=2.0)
+              #  m.move(x=-1920, y=1080)
+               # time.sleep(1)
+    #time.sleep(1)
